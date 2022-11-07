3f9faf4bc6a4f58435ebe97b66bd7e2047c874f7
Ben Hollamby <ben.hollamby@outlook.com>
2022-11-07 14:49:22 +1300

    toying

--- Network/NSG/nsg.tf
+++ Network/NSG/nsg.tf
@@ -4,15 +4,15 @@ resource "azurerm_network_security_group" "nsg" {
   location = var.Location
 
   security_rule {
-    name                       = "All_Traffic"
-    priority                   = 140
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = var.External_Next_Hop
+      name                       = "All_Traffic"
+      priority                   = 140
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "*"
+      source_address_prefix      = "*"
+      destination_address_prefix = var.External_Next_Hop
   }
 
   security_rule {
